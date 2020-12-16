06103d56ea7fed237f83fbbd6691d6bea2fc767f
Alejandro <alejandro23aq01@hotmail.com>
2020-12-16 17:39:16 -0600

    move-fd

--- move-fd.py
+++ move-fd.py
@@ -1,19 +1,26 @@
 # programa scripting para mover archivos y carpetas.
+# scripting program to move files and folders.
 import os
 import shutil
 import argparse
 
+
 def mover_archivo(archive, output):
     if os.path.exists(archive) and os.path.exists(output):
         shutil.move(archive, output)
-        print(f"{archive} se ha movido!")
+        print(f"{archive} has been moved!")
+    elif not os.path.exists(archive):
+        print(f"{archive} not found!")
+    elif not os.path.exists(output):
+        print(f"{output} does not exist!")
     else:
-        print(f"{archive} no encontrado o {output} no existe!")
+        print("???")
 
+# example: python move-fd.py -x "file.txt" -o "c:\Users\PROFILE\Desktop"
+parser = argparse.ArgumentParser(description='script to easily move files from one place to another')
+parser.add_argument('-x', '--archive', type=str, metavar='', required=True, help='file or folder name, example -x "one.txt"')
+parser.add_argument('-o', '--output', type=str, metavar='', required=True, help='output example: -o "{}"'.format(os.getcwd()))
 
-parser = argparse.ArgumentParser(description="script para mover archivos fácilmente de un lugar a otro.")
-parser.add_argument('-x', '--archive', type=str, metavar='', required=True,help="nombre del archivo o carpeta, la 'dirección' es opcional")
-parser.add_argument('-o', '--output', type=str, metavar='', required=True, help="ubicación de salida")
 args = parser.parse_args()
 
 if __name__ == '__main__':
